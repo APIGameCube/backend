c4125b4d1319db27935b5e4b7a9fd32adfed2456
Mohammad-Shahin23 <mohammad.shahin2221@gmail.com>
2023-03-25 15:28:32 +0000

    free-to-play API tested succesfuly

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
 'use strict'
 
-const express = require ('express');
-const cors = require ('cors');
-const pg = require ('pg');
-const axios = require ('axios');
+const express = require('express');
+const cors = require('cors');
+const pg = require('pg');
+const axios = require('axios');
 require('dotenv').config();
 
 const server = express();
 server.use(cors());
 server.use(express.json()); //read request body
+
 const PORT = process.env.PORT || 3000;
 
 //ROUTES
 server.get('/', homeHandler);
+server.get('/SearchMusic', SearchMusicHandler)// this handler's perpous is to fetch all the data.
+// server.use(errorHandler);
 
-function homeHandler(req,res) {
+function homeHandler(req, res) {
     res.status(200).send('HOME')
 }
 
-server.listen(PORT , () => {
+function SearchMusicHandler(request, res) {
+
+
+
+    const options = {
+        method: 'GET',
+        url: 'https://free-to-play-games-database.p.rapidapi.com/api/games',
+        headers: {
+            'X-RapidAPI-Key': '04df37f83fmsh09e19e247d3adefp12b631jsnfc3f7ce0007e',
+            'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+        }
+    };
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+        res.send(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+
+
+}
+
+
+// function errorHandler(error, req, res) {
+//     const err = {
+//         status: 500,
+//         message: error
+//     }
+//     res.status(500).send(err);
+// }
+
+server.listen(PORT, () => {
     console.log(`hii on ${PORT}`);
 })
---
+++ schema.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS favFreeGame;
+
+CREATE TABLE IF NOT EXISTS favFreeGame (
+    id  SERIAL PRIMARY KEY,
+    title  VARCHAR(255),
+    thumbnail VARCHAR(10000),
+    genre VARCHAR(255),
+    platform VARCHAR(255),
+    publisher  VARCHAR(255),
+    developer  VARCHAR(255),
+    release_date  VARCHAR(255),
+    short_description VARCHAR(10000)
+
+);
