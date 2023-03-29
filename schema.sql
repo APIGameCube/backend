5d6c3b368c775a9508e9afe447836cc14f0f8a4a
Nawrssama <nawrsbibi.nb@gmail.com>
2023-03-29 08:29:53 +0000

    changed port

--- schema.sql
+++ schema.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS favFreeGame;
 
 CREATE TABLE IF NOT EXISTS favFreeGame (
     id  SERIAL PRIMARY KEY,
+    userid VARCHAR(255),
 
     title  VARCHAR(255),
     thumbnail VARCHAR(10000),
@@ -16,3 +17,10 @@ CREATE TABLE IF NOT EXISTS favFreeGame (
    
 
 );
+
+-- CREATE TABLE IF NOT EXISTS users (
+--     userid SERIAL PRIMARY KEY,
+--     username VARCHAR(255),
+--     password VARCHAR(255),
+--     email VARCHAR(255)
+-- );
